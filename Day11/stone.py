41883c51c11d6af6870dd79be65fe6c4952cf0d5
Etienne <etienne.bardet@gmail.com>
2024-12-11 14:40:23 +0100

    part 2

--- Day11/stone.py
+++ Day11/stone.py
@@ -1,51 +1,55 @@
 import numpy as np
 from tqdm import tqdm
-from multiprocessing import Pool
+from functools import cache
 
-
+@cache
 def blink(stones):
     new_stones = []
     for stone in stones:
         if stone == '0':
             new_stones.append('1')
-        elif len(stone)%2 == 0:
+        elif len(stone) % 2 == 0:
             stone1 = stone[:len(stone)//2]
             stone2 = stone[len(stone)//2:]
 
             new_stones.append(str(int(stone1)))
             new_stones.append(str(int(stone2)))
         else:
-            new_stones.append(str(int(stone)*2024))
+            new_stones.append(str(int(stone) * 2024))
 
-    return new_stones
+    return tuple(new_stones)
 
-def blink_recursively(stones, i=0):
-    print(i)
-    if i == 50:
-        return len(stones)
+def blink_recursively(stones, iterations):
+    if iterations == 0:
+        return stones
+    return blink_recursively(blink(stones), iterations - 1)
+
+@cache
+def blink2(stone, steps):
+    if steps == 0:
+        return 1
+    if stone == '0':
+        return blink2('1', steps - 1)
+    length = len(stone)
+    if length%2==0:
+        return blink2(str(int(stone[:length//2])),steps-1) + blink2(str(int(stone[length//2:])), steps - 1)
+    return blink2(str(int(stone) * 2024), steps - 1)
     
-    total_len = 0
-    new_stones = blink(stones)
-    total_len = blink_recursively(new_stones, i + 1)
-    return total_len
-
 def main():
     with open('./Day11/file.txt') as f:
         lines = f.readlines()
     lines = [line.strip() for line in lines]    
 
-    sum=0
     for i in range(len(lines)):
         lines[i] = lines[i].strip('\n')
         lines[i] = lines[i].split(' ')
         to_blink = lines[i]
-        for i in range(25):
-            to_blink = blink(to_blink)
+        to_blink = blink_recursively(tuple(to_blink), 3)
+
 
     print("Answer to part 1: ", len(to_blink))
-    
-    for b in to_blink:
-        sum2 = blink_recursively(b)
+
+    sum2 = sum(blink2(stone, 75) for stone in lines[0])
     print("Answer to part 2: ", sum2)
 
 if __name__ == '__main__':
